f74eee7807731ffc7da30166b2860b453d7d91bd
Kenny Stephens <34525009+KennyStephens@users.noreply.github.com>
2019-01-08 08:21:06 -0700

    Destructuring

--- js refresher/play.js
+++ js refresher/play.js
@@ -32,27 +32,40 @@ const person = {
     }
 };
 
-const copiedPerson = {...person};
-console.log(copiedPerson);
+const printName = ({ name }) => {
+    console.log(name);
+};
+
+printName(person);
+
+const { name, age } = person;
+console.log(name, age);
+
+// const copiedPerson = {...person};
+// console.log(copiedPerson);
 
 // console.log(person);
 // person.greet();
 
 const hobbies = ['Sports', 'Cooking'];
-// for (let hobby of hobbies) {
-//     console.log(hobby);
-// }
+const [hobby1, hobby2] = hobbies;
+console.log(hobby1, hobby2);
 
-// console.log(hobbies.map(hobby => 'Hobby: ' + hobby));
-// console.log(hobbies);
-// hobbies.push('Programming');
-// console.log(hobbies);
+// // for (let hobby of hobbies) {
+// //     console.log(hobby);
+// // }
 
-const copiedArray = [...hobbies];
-console.log(copiedArray);
+// // console.log(hobbies.map(hobby => 'Hobby: ' + hobby));
+// // console.log(hobbies);
+// // hobbies.push('Programming');
+// // console.log(hobbies);
 
-const toArray = (...args) => {
-    return args;
-};
+// const copiedArray = [...hobbies];
+// console.log(copiedArray);
 
-console.log(toArray(1, 2, 3, 4));
+// const toArray = (...args) => {
+//     return args;
+// };
+
+// console.log(toArray(1, 2, 3, 4));
+
