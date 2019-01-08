e11be5fa789b6413a93351967e2f285314aa5340
Kenny Stephens <34525009+KennyStephens@users.noreply.github.com>
2019-01-08 08:08:17 -0700

    Reference types

--- js refresher/play.js
+++ js refresher/play.js
@@ -40,5 +40,9 @@ const hobbies = ['Sports', 'Cooking'];
 //     console.log(hobby);
 // }
 
-console.log(hobbies.map(hobby => 'Hobby: ' + hobby));
+// console.log(hobbies.map(hobby => 'Hobby: ' + hobby));
+// console.log(hobbies);
+
+hobbies.push('Programming');
+
 console.log(hobbies);
