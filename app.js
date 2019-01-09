122cc2a5fb0d8dedd25c623184b7cb61ac5bf354
Kenny Stephens <34525009+KennyStephens@users.noreply.github.com>
2019-01-09 09:41:42 -0700

    Working with responses to get form data

--- app.js
+++ app.js
@@ -1,8 +1,35 @@
 const http = require('http');
+const fs = require('fs');
 
 // Called by node.js whenever a request is received
 const server = http.createServer((request, response) => {
-    console.log(request.url, request.method, request.headers);
+    const url = request.url;
+    const method = request.method;
+    if(url === '/') {
+        response.setHeader('Content-Type', 'text/html');
+        response.write('<html>')
+        response.write('<head><title>Enter Message</title></head>');
+        response.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
+        response.write('</html>')
+        return response.end();
+    }
+    if(url === '/message' && method === 'POST') {
+        const body = [];
+        request.on('data', chunk => {
+            console.log(chunk);
+            body.push(chunk);
+        });
+        request.on('end', () => {
+            const parsedBody = Buffer.concat(body).toString();
+            console.log(parsedBody);
+            const message = parsedBody.split('=')[1];
+            fs.writeFileSync('message.txt', message);
+        });
+        response.statusCode = 302;
+        response.setHeader('Location', '/');
+        return response.end();
+    }
+    // console.log(request.url, request.method, request.headers);
     // process.exit();
     response.setHeader('Content-Type', 'text/html');
     response.write('<html>')
