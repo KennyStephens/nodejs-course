d38b11c101ec405c35fbe3162788f67ffc6016c9
Kenny Stephens <kennethcstephens@gmail.com>
2019-01-09 15:58:26 -0700

    Tried adding new things to header

--- DGM4790 - PRACTICE/main.js
+++ DGM4790 - PRACTICE/main.js
@@ -3,15 +3,13 @@ const http = require('http');
 
 const server = http.createServer((request, response) => {
   console.log('Server created');
-  response.setHeader('Content-Type', 'text/html');
+  // response.setHeader('Content-Type', 'text/html');
+  response.setHeader('Thor', 'Thor is awesome');
   response.write('<html>');
   response.write('<head><title>My Server</title></head>');
   response.write('<body><h1>Welcome to my server!</h1></body>')
-
   response.write('</html>');
   return response.end();
 });
 
 server.listen(3000);
-
-
