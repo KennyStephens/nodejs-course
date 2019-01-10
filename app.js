54715bff79ff864567cb05e4f95ff6461553f43a
Kenny Stephens <34525009+KennyStephens@users.noreply.github.com>
2019-01-10 08:28:46 -0700

    Re-structured and worked with modules

--- app.js
+++ app.js
@@ -1,44 +1,10 @@
 const http = require('http');
-const fs = require('fs');
+
+const routes = require('./routes')
+
+console.log(routes.someText);
 
 // Called by node.js whenever a request is received
-const server = http.createServer((request, response) => {
-    const url = request.url;
-    const method = request.method;
-    if(url === '/') {
-        response.setHeader('Content-Type', 'text/html');
-        response.write('<html>')
-        response.write('<head><title>Enter Message</title></head>');
-        response.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
-        response.write('</html>')
-        return response.end();
-    }
-    if(url === '/message' && method === 'POST') {
-        const body = [];
-        request.on('data', chunk => {
-            console.log(chunk);
-            body.push(chunk);
-        });
-        return request.on('end', () => {
-            const parsedBody = Buffer.concat(body).toString();
-            console.log(parsedBody);
-            const message = parsedBody.split('=')[1];
-            fs.writeFile('message.txt', message, err => {
-                response.statusCode = 302;
-                response.setHeader('Location', '/');
-                return response.end();
-            });
-
-        });
-    }
-    // console.log(request.url, request.method, request.headers);
-    // process.exit();
-    response.setHeader('Content-Type', 'text/html');
-    response.write('<html>')
-    response.write('<head><title>My First Page</title></head>');
-    response.write('<body><h1>Hello from my Node.js server</h1></body>');
-    response.write('</html>')
-    response.end();
-});
+const server = http.createServer(routes.handler);
 
 server.listen(3000);
---
+++ routes.js
@@ -0,0 +1,55 @@
+const fs = require('fs');
+
+const requestHandler = (request, response) => {
+    const url = request.url;
+    const method = request.method;
+
+    if(url === '/') {
+        response.setHeader('Content-Type', 'text/html');
+        response.write('<html>')
+        response.write('<head><title>Enter Message</title></head>');
+        response.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
+        response.write('</html>')
+        return response.end();
+    }
+    if(url === '/message' && method === 'POST') {
+        const body = [];
+        request.on('data', chunk => {
+            console.log(chunk);
+            body.push(chunk);
+        });
+        return request.on('end', () => {
+            const parsedBody = Buffer.concat(body).toString();
+            console.log(parsedBody);
+            const message = parsedBody.split('=')[1];
+            fs.writeFile('message.txt', message, err => {
+                response.statusCode = 302;
+                response.setHeader('Location', '/');
+                return response.end();
+            });
+    
+        });
+    }
+    // console.log(request.url, request.method, request.headers);
+    // process.exit();
+    response.setHeader('Content-Type', 'text/html');
+    response.write('<html>')
+    response.write('<head><title>My First Page</title></head>');
+    response.write('<body><h1>Hello from my Node.js server</h1></body>');
+    response.write('</html>')
+    response.end();
+};
+
+// For Exporting
+
+// module.exports = requestHandler;
+
+// For multiple exports
+// module.exports = {
+//     handler: requestHandler,
+//     someText: 'Some hard coded text'
+// };
+
+// A different way to do multiple exports
+exports.handler = requestHandler;
+exports.someText = 'Some hard coded text';
