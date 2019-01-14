3c7157b4bd3bc6045749e081fec8688abe2e642e
Kenny Stephens <34525009+KennyStephens@users.noreply.github.com>
2019-01-14 08:14:26 -0700

    Working with middlewares

--- app.js
+++ app.js
@@ -2,6 +2,19 @@ const http = require('http');
 
 const express = require('express');
 
-const server = http.createServer();
+const app = express();
+
+// app.use will run for every incoming request
+app.use((req, res, next) => {
+    console.log('In the middleware!');
+    next(); // Allows the request to continue to the next middleware in line
+});
+
+app.use((req, res, next) => {
+    console.log('In another middleware!');
+    // ...
+});
+
+const server = http.createServer(app);
 
 server.listen(3000);
