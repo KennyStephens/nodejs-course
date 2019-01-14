1a5cb7796e83bd817de72b291099f332e55964b4
Kenny Stephens <34525009+KennyStephens@users.noreply.github.com>
2019-01-14 09:45:52 -0700

    Updated

--- Assignment 2/app.js
+++ Assignment 2/app.js
@@ -2,25 +2,25 @@ const express = require('express');
 
 const app = express();
 
-// app.use((req, res, next) => {
-//   console.log('First Middleware');
-//     next();
-// });
-
-// app.use((req, res, next) => {
-//     console.log('Second Middleware');
-//     res.send('<h1>Assignment Solved (almost!)</h1>');
-// });
-
-app.use('/users', (req, res, next) => {
-    console.log('/users middleware');
-    res.send('<p>The Middleware that handles just /users</p>');
+app.use((req, res, next) => {
+  console.log('First Middleware');
+    next();
 });
 
-app.use('/', (req, res, next) => {
-    console.log('/ middleware');
-    res.send('<p>The Middleware that handles just /</p>');
+app.use((req, res, next) => {
+    console.log('Second Middleware');
+    res.send('<h1>Assignment Solved (almost!)</h1>');
 });
+
+// app.use('/users', (req, res, next) => {
+//     console.log('/users middleware');
+//     res.send('<p>The Middleware that handles just /users</p>');
+// });
+
+// app.use('/', (req, res, next) => {
+//     console.log('/ middleware');
+//     res.send('<p>The Middleware that handles just /</p>');
+// });
 
 
 app.listen(3000);
