dafbd492f96a5d4752eed8b739942a54ae23a222
Kenny Stephens <34525009+KennyStephens@users.noreply.github.com>
2019-01-15 07:57:52 -0700

    Added 404 page

--- routes/shop.js
+++ routes/shop.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 const express = require('express');
 
 const router = express.Router();
@@ -5,7 +7,7 @@ const router = express.Router();
 // app.use will run for every incoming request
 router.get('/', (req, res, next) => {
   // console.log('In another middleware!');
-  res.send('<h1>Hello from Express!</h1>');
+  res.sendFile(path.join(__dirname,'..', 'views', 'shop.html'));
 });
 
 module.exports = router;
