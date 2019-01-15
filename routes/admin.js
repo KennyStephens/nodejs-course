038218dd265a1e4d70af1ff6810452ba5f79338b
Kenny Stephens <34525009+KennyStephens@users.noreply.github.com>
2019-01-15 08:02:06 -0700

    Creating rootDir helper variable

--- routes/admin.js
+++ routes/admin.js
@@ -2,13 +2,15 @@ const path = require('path');
 
 const express = require('express');
 
+const rootDir = require('../util/path');
+
 const router = express.Router();
 
 // app.use will run for every incoming request
 // /admin/add-product => GET
 router.get('/add-product', (req, res, next) => {
   // console.log('In another middleware!');
-  res.sendFile(path.join(__dirname,'..', 'views', 'add-product.html'));
+  res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
 });
 
 // /admin/add-product => POST
