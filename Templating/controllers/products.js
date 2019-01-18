58830256802469dc4f5d9946553b734c9b5c4d01
Kenny Stephens <34525009+KennyStephens@users.noreply.github.com>
2019-01-18 09:36:51 -0700

    Using models

--- Templating/controllers/products.js
+++ Templating/controllers/products.js
@@ -1,27 +1,29 @@
-const products = [];
+const Product = require('../models/product');
 
 exports.getAddProduct = (req, res, next) => {
     res.render('add-product', {
-      pageTitle: 'Add Product', 
-      path: '/admin/add-product', 
-      productCSS: true, 
-      formsCSS: true, 
-      activeAddProduct: true
+        pageTitle: 'Add Product',
+        path: '/admin/add-product',
+        productCSS: true,
+        formsCSS: true,
+        activeAddProduct: true
     });
-  }
+}
 
-  exports.postAddProduct = (req, res, next) => {
-    products.push({title: req.body.title});
+exports.postAddProduct = (req, res, next) => {
+    const product = new Product(req.body.title);
+    product.save();
     res.redirect('/');
-  }
+}
 
-  exports.getProducts = (req, res, next) => {
+exports.getProducts = (req, res, next) => {
+    const products = Product.fetchAll();
     res.render('shop', {
-      prods: products, 
-      pageTitle: 'Shop', 
-      path: '/', 
-      hasProducts: products.length > 0,
-      activeShop: true,
-      productCSS: true
+        prods: products,
+        pageTitle: 'Shop',
+        path: '/',
+        hasProducts: products.length > 0,
+        activeShop: true,
+        productCSS: true
     });
-  };
+};
