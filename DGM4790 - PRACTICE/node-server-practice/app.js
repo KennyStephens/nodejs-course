420285fbe778f41c18237ddccc7ecb53595b32fd
Kenny Stephens <kennethcstephens@gmail.com>
2019-01-25 15:36:49 -0700

    Connecting to db

--- DGM4790 - PRACTICE/node-server-practice/app.js
+++ DGM4790 - PRACTICE/node-server-practice/app.js
@@ -2,11 +2,19 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
-const dev_db_url = 'mongodb+srv://Kenknee:Node-server-practice@product-cluster-ts8li.gcp.mongodb.net/admin';
+const dev_db_url = 'mongodb+srv://Kenknee:Node-server-practice@product-cluster-ts8li.gcp.mongodb.net/product';
+const mongoDB = process.env.MONGODB_URI || dev_db_url;
+mongoose.connect(mongoDB, {useNewUrlParser: true});
+mongoose.Promise = global.Promise;
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 const product = require('./routes/product.route');
 
 const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.use('/products', product);
 
