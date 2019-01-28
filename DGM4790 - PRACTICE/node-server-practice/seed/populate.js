c8a271111fb1905c72c7a49b0b40efd05b4dec75
Kenny Stephens <kennethcstephens@gmail.com>
2019-01-28 15:21:01 -0700

    working with pokemon api

---
+++ DGM4790 - PRACTICE/node-server-practice/seed/populate.js
@@ -0,0 +1,10 @@
+const fetch = require('node-fetch');
+
+fetch('https://pokeapi.co/api/v2/pokemon/?limit=25')
+  .then(response => {
+    return response.json();
+  })
+  .then(myJson => {
+    // console.log(myJson.results);
+    const pokeArray = myJson.results;
+  });
