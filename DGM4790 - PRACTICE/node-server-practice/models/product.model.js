4444726d56e2459ff52561fbda6a1c3ab94701e2
Kenny Stephens <kennethcstephens@gmail.com>
2019-01-28 15:25:35 -0700

    Created a pokemon schema

--- DGM4790 - PRACTICE/node-server-practice/models/product.model.js
+++ DGM4790 - PRACTICE/node-server-practice/models/product.model.js
@@ -7,4 +7,10 @@ const ProductSchema = new Schema({
   price: {type: Number, required: true}
 });
 
+const PokemonSchema = new Schema({
+  name: { type: String, required: true },
+  url: { type: String, required: true }
+});
+
 module.exports =  mongoose.model('Product', ProductSchema);
+module.exports =  mongoose.model('Pokemon', PokemonSchema);
