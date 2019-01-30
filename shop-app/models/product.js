cb7cbbc5e46bd9243f9fca883d2e556bc471efd8
Kenny Stephens <kennethcstephens@gmail.com>
2019-01-29 19:52:28 -0700

    trying to fix deprecation error

--- shop-app/models/product.js
+++ shop-app/models/product.js
@@ -10,24 +10,30 @@ class Product {
 
   save() {
     const db = getDb();
-    return db.collection('products')
+    return db
+      .collection('products')
       .insertOne(this)
       .then(result => {
         console.log(result);
       })
-      .catch(err => console.log(err));
+      .catch(err => {
+        console.log(err);
+      });
   }
 
   static fetchAll() {
     const db = getDb();
-    return db.collection('products')
+    return db
+      .collection('products')
       .find()
       .toArray()
       .then(products => {
         console.log(products);
         return products;
       })
-      .catch(err => console.log(err));
+      .catch(err => {
+        console.log(err);
+      });
   }
 }
 
--- shop-app/util/database.js
+++ shop-app/util/database.js
@@ -3,28 +3,27 @@ const MongoClient = mongodb.MongoClient;
 
 let _db;
 
-const mongoConnect = (callback) => {
-  MongoClient.connect('mongodb+srv://kenkneesteefens:4PVGlYV0HuZ3k7vM@cluster0-drydi.mongodb.net/shop?retryWrites=true')
-  .then(client => {
-    console.log('Connected!');
-    _db = client.db();
-    callback();
-  })
-  .catch(err => {
-    console.log(err)
-    throw err;
-  });
+const mongoConnect = callback => {
+  MongoClient.connect(
+    'mongodb+srv://maximilian:9u4biljMQc4jjqbe@cluster0-ntrwp.mongodb.net/shop?retryWrites=true', {useNewUrlParser: true}
+  )
+    .then(client => {
+      console.log('Connected!');
+      _db = client.db();
+      callback();
+    })
+    .catch(err => {
+      console.log(err);
+      throw err;
+    });
 };
 
 const getDb = () => {
-  if(_db) {
+  if (_db) {
     return _db;
   }
-  throw 'No Database found!';
+  throw 'No database found!';
 };
 
 exports.mongoConnect = mongoConnect;
 exports.getDb = getDb;
-
-
-
