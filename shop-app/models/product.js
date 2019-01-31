64164618181845d5993ca50ad80335073f41aaae
Kenny Stephens <kennethcstephens@gmail.com>
2019-01-30 19:27:00 -0700

    editing products

--- shop-app/models/product.js
+++ shop-app/models/product.js
@@ -2,18 +2,30 @@ const mongodb = require('mongodb');
 const getDb = require('../util/database').getDb;
 
 class Product {
-  constructor(title, price, description, imageUrl) {
+  constructor(title, price, description, imageUrl, id) {
     this.title = title;
     this.price = price;
     this.description = description;
     this.imageUrl = imageUrl;
+    this._id = id;
   }
 
   save() {
     const db = getDb();
-    return db
-      .collection('products')
-      .insertOne(this)
+    let dbOp;
+    if (this._id) {
+      // Update the Product
+      dbOp = db.collection('products').updateOne({
+        _id: mongodb.ObjectId(this._id)
+      }, {
+        $set: this
+      });
+    } else {
+      dbOp = db
+        .collection('products')
+        .insertOne(this);
+    }
+    return dbOp
       .then(result => {
         console.log(result);
       })
@@ -40,7 +52,9 @@ class Product {
   static findById(prodId) {
     const db = getDb();
     return db.collection('products')
-      .find({_id: new mongodb.ObjectId(prodId) })
+      .find({
+        _id: new mongodb.ObjectId(prodId)
+      })
       .next()
       .then(product => {
         console.log(product);
