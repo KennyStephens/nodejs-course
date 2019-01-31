ec2576abaa9e4708a70580cde605310c26e628c6
Kenny Stephens <kennethcstephens@gmail.com>
2019-01-31 15:15:58 -0700

    User stuff

--- shop-app/app.js
+++ shop-app/app.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
 const errorController = require('./controllers/error');
-// const User = require('./models/user');
+const User = require('./models/user');
 
 const app = express();
 
@@ -15,17 +15,19 @@ app.set('views', 'views');
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use((req, res, next) => {
-//   User.findById('5baa2528563f16379fc8a610')
-//     .then(user => {
-//       req.user = new User(user.name, user.email, user.cart, user._id);
-//       next();
-//     })
-//     .catch(err => console.log(err));
-// });
+app.use((req, res, next) => {
+  User.findById('5c537386ad70f408d705bab5')
+    .then(user => {
+      req.user = user;
+      next();
+    })
+    .catch(err => console.log(err));
+});
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
@@ -34,8 +36,22 @@ app.use(errorController.get404);
 
 mongoose
   .connect(
-    'mongodb+srv://kenkneesteefens:Nodecourse@cluster0-drydi.mongodb.net/shop?retryWrites=true', { useNewUrlParser: true })
+    'mongodb+srv://kenkneesteefens:Nodecourse@cluster0-drydi.mongodb.net/shop?retryWrites=true', {
+      useNewUrlParser: true
+    })
   .then(result => {
+    User.findOne().then(user => {
+      if (!user) {
+        const user = new User({
+          name: 'Max',
+          email: 'max@test.com',
+          cart: {
+            items: []
+          }
+        });
+        user.save();
+      }
+    });
     app.listen(3000);
   })
   .catch(err => {
