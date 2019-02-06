fabe7978844c27939dd35b917863bbe7ed459692
Kenny Stephens <kennethcstephens@gmail.com>
2019-02-05 20:20:06 -0700

    Change

--- store-app-login/app.js
+++ store-app-login/app.js
@@ -20,7 +20,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
-  User.findById('')
+  User.findById('5c537386ad70f408d705bab5')
     .then(user => {
       req.user = user;
       next();
