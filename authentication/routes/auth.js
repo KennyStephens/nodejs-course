44b219d86b111dfd3b12a178a5e13487ce4e93a3
Kenny Stephens <kennethcstephens@gmail.com>
2019-02-09 10:27:41 -0700

    custom validator

--- authentication/routes/auth.js
+++ authentication/routes/auth.js
@@ -15,7 +15,14 @@ router.post('/login', authController.postLogin);
 
 router.post('/signup', check('email')
   .isEmail()
-  .withMessage('Please enter a valid email.'), authController.postSignup);
+  .withMessage('Please enter a valid email.')
+  .custom((value, { req }) => {
+    if(value === 'test@test.com') {
+      throw new Error('This email address is forbidden');
+    }
+    return true;
+  }), 
+  authController.postSignup);
 
 router.post('/logout', authController.postLogout);
 
