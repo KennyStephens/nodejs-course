2dd54f986f890f44f5e7634165fba936ea209b4a
Kenny Stephens <34525009+KennyStephens@users.noreply.github.com>
2019-02-12 14:13:27 -0700

    Sending data

--- REST APIs/routes/feed.js
+++ REST APIs/routes/feed.js
@@ -7,5 +7,8 @@ const router = express.Router();
 // GET /feed/posts
 router.get('/posts', feedController.getPosts);
 
+// POST /feed/post
+router.post('/post', feedController.createPost);
+
 
 module.exports = router;
