1665f6e0170739862bf719523a8e5309c87c2118
Kenny Stephens <kennethcstephens@gmail.com>
2019-02-11 20:02:35 -0700

    setting filetype in headers

--- authentication/controllers/shop.js
+++ authentication/controllers/shop.js
@@ -147,12 +147,14 @@ exports.getOrders = (req, res, next) => {
 
 exports.getInvoice = (req, res, next) => {
   const orderId = req.params.orderId;
-  const invoiceName = 'invoice-' + orderId + 'pdf';
+  const invoiceName = 'invoice-' + orderId + '.pdf';
   const invoicePath = path.join('data', 'invoices', invoiceName);
   fs.readFile(invoicePath, (err, data) => {
     if(err) {
       return next(err);
     }
+    res.setHeader('Content-type', 'application/pdf');
+    res.setHeader('Content-Disposition', 'inline; filename="' + invoiceName + '"');
     res.send(data);
   });
 };
