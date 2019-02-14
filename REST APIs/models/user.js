0e316eaa5cbe798b28ff56af082094b7d61af816
Kenny Stephens <kennethcstephens@gmail.com>
2019-02-14 15:49:37 -0700

    Signing users up

--- REST APIs/models/user.js
+++ REST APIs/models/user.js
@@ -16,12 +16,14 @@ const userSchema = new Schema({
   },
   status: {
     type: String,
-    required: true
+    default: 'I am new!'
   },
-  posts: [{
-    type: Schema.Types.ObjectId,
-    ref: 'Post'
-  }]
+  posts: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'Post'
+    }
+  ]
 });
 
 module.exports = mongoose.model('User', userSchema);
--- REST APIs/routes/auth.js
+++ REST APIs/routes/auth.js
@@ -7,27 +7,29 @@ const authController = require('../controllers/auth');
 
 const router = express.Router();
 
-router.put('/signup', [
-  body('email')
-  .isEmail()
-  .withMessage('Please enter a valid email')
-  .custom((value, {
-    req
-  }) => {
-    return User.findOne({
-        email: value
+router.put(
+  '/signup',
+  [
+    body('email')
+      .isEmail()
+      .withMessage('Please enter a valid email.')
+      .custom((value, { req }) => {
+        return User.findOne({ email: value }).then(userDoc => {
+          if (userDoc) {
+            return Promise.reject('E-Mail address already exists!');
+          }
+        });
       })
-      .then(userDoc => {
-        if (userDoc) {
-          return Promsise.reject('E-Mail address already exists!');
-        }
-      })
-  })
-  .normalizeEmail(),
-  body('password').trim().isLength({
-    min: 5
-  }),
-  body('name').trim().not().isEmpty()
-], authController.signup);
+      .normalizeEmail(),
+    body('password')
+      .trim()
+      .isLength({ min: 5 }),
+    body('name')
+      .trim()
+      .not()
+      .isEmpty()
+  ],
+  authController.signup
+);
 
 module.exports = router;
