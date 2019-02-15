b3606c59c93eaed8da5cdd0ff3c872c05cb6e86f
Kenny Stephens <kennethcstephens@gmail.com>
2019-02-14 17:07:01 -0700

    Loggin in and web tokens

--- REST APIs/controllers/auth.js
+++ REST APIs/controllers/auth.js
@@ -2,6 +2,7 @@ const {
   validationResult
 } = require('express-validator/check');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const User = require('../models/user');
 
@@ -28,7 +29,10 @@ exports.signup = (req, res, next) => {
       return user.save();
     })
     .then(result => {
-      res.status(201).json({ message: 'User created!', userId: result._id });
+      res.status(201).json({
+        message: 'User created!',
+        userId: result._id
+      });
     })
     .catch(err => {
       if (!err.statusCode) {
@@ -38,13 +42,15 @@ exports.signup = (req, res, next) => {
     });
 };
 
-export.login = (req, res, next) => {
+exports.login = (req, res, next) => {
   const email = req.body.email;
   const password = req.body.password;
   let loadedUser;
-  User.findOne({email: email})
+  User.findOne({
+      email: email
+    })
     .then(user => {
-      if(!user) {
+      if (!user) {
         const error = new Error('A user with this email could not be found.');
         error.statusCode = 401;
         throw error;
@@ -53,12 +59,18 @@ export.login = (req, res, next) => {
       return bcrypt.compare(password, user.password);
     })
     .then(isEqual => {
-      if(!isEqual) {
+      if (!isEqual) {
         const error = new Error('Wrong password');
         error.statusCode = 401;
         throw error;
       }
-      
+      const token = jwt.sign({
+        email: loadedUser.email,
+        userId: loadedUser._id.toString()
+      }, 'secret', {
+        expiresIn: '1hr'
+      });
+      res.status(200).json({token: token, userId: loadedUser._id.toString()});
     })
     .catch(err => {
       if (!err.statusCode) {
