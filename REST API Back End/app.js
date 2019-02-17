a34d6b4c0f7ab3c451fece30b9cd29af1caa509a
Kenny Stephens <kennethcstephens@gmail.com>
2019-02-17 13:00:54 -0700

    connecting graphql stuff

--- REST API Back End/app.js
+++ REST API Back End/app.js
@@ -35,10 +35,7 @@ const fileFilter = (req, file, cb) => {
 // app.use(bodyParser.urlencoded()); // x-www-form-urlencoded <form>
 app.use(bodyParser.json()); // application/json
 app.use(
-  multer({
-    storage: fileStorage,
-    fileFilter: fileFilter
-  }).single('image')
+  multer({ storage: fileStorage, fileFilter: fileFilter }).single('image')
 );
 app.use('/images', express.static(path.join(__dirname, 'images')));
 
@@ -49,41 +46,43 @@ app.use((req, res, next) => {
     'OPTIONS, GET, POST, PUT, PATCH, DELETE'
   );
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200);
+  }
   next();
 });
 
-app.use('/graphql', graphqlHttp({
-  schema: graphqlSchema,
-  rootValue: graphqlResolver,
-  graphiql: true,
-  formatError(err) {
-    if (!err.originalError) {
-      return err;
+app.use(
+  '/graphql',
+  graphqlHttp({
+    schema: graphqlSchema,
+    rootValue: graphqlResolver,
+    graphiql: true,
+    formatError(err) {
+      if (!err.originalError) {
+        return err;
+      }
+      const data = err.originalError.data;
+      const message = err.message || 'An error occurred.';
+      const code = err.originalError.code || 500;
+      return { message: message, status: code, data: data };
     }
-    const data = err.originalError.data;
-    const message = data.message || ' An error occured.';
-    const code = err.originalError.code || 500;
-    return { message: message, status: code, data: data };
-  }
-}));
+  })
+);
 
 app.use((error, req, res, next) => {
   console.log(error);
   const status = error.statusCode || 500;
   const message = error.message;
   const data = error.data;
-  res.status(status).json({
-    message: message,
-    data: data
-  });
+  res.status(status).json({ message: message, data: data });
 });
 
 mongoose
   .connect(
     'mongodb+srv://kenkneesteefens:Nodecourse@cluster0-drydi.mongodb.net/messages', {
       useNewUrlParser: true
-    }
-  )
+    })
   .then(result => {
     app.listen(8080);
   })
